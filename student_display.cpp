17e3f5ea8f326b2d02b4ea16e04244ad83206555
pradnesh7117 <pradnesh7581@gmail.com>
2026-09-16 21:57:41 +0530

    Create student_display.cpp

---
+++ student_display.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int roll1,roll2,roll3,roll4,roll5;
+
+    cout<<"enter student 1 roll number:"<<endl;
+    cin>>roll1;
+    cout<<"enter student 2 roll number:"<<endl;
+    cin>>roll2;
+    cout<<"enter student 3 roll number:"<<endl;
+    cin>>roll3;
+    cout<<"enter student 4 roll number:"<<endl;
+    cin>>roll4;
+    cout<<"enter student 5 roll number:"<<endl;
+    cin>>roll5;
+
+
+    cout<<"the roll number of student 1 is:"<<roll1<<"\n";
+    cout<<"the roll number of student 2 is:"<<roll2<<"\n";
+    cout<<"the roll number of student 3 is:"<<roll3<<"\n";
+    cout<<"the roll number of student 4 is:"<<roll4<<"\n";
+    cout<<"the roll number of student 5 is:"<<roll5<<"\n";
+   return 0;
+} 
